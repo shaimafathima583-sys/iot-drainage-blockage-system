4108f02368f2f3ee70b7aab7e178a00fd0c88199
Nabila F <17nabilafahim@gmail.com>
2026-03-02 03:21:54 +0530

    Update main.ino
    add firebase layer

--- esp32-firmware/main.ino
+++ esp32-firmware/main.ino
@@ -1,17 +1,29 @@
 #include <WiFi.h>
+#include <Firebase_ESP_Client.h>
 
 const char* ssid = "Nphn";
 const char* pwd = "";
+
+#define DATABASE_URL "https://your-project.firebaseio.com/"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 void setup() 
 {
   Serial.begin(115200);
   WiFi.begin(ssid, pwd);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("WiFi connected");
+  
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true)
+
 }
 
-void loop() {}
+void loop() {
+Firebase.RTDB.setInt(&fbdo, "/drains/DRAIN_001/live/ultrasonic", random(10, 50));
+  delay(1000);
+}
